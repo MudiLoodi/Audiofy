5cc26c76878d81fcf7058b714e8eb6723c904baf
MudiLoodi <54457769+MudiLoodi@users.noreply.github.com>
2025-04-17 23:51:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 from gui.app import AudioBookApp
 
 if __name__ == "__main__":
-    ctk.set_appearance_mode("Dark")  # Set dark mode for better visibility
-    ctk.set_default_color_theme("blue")  # Use blue theme for consistency
+    ctk.set_appearance_mode("Dark")  
+    ctk.set_default_color_theme("blue") 
     app = AudioBookApp()
     app.mainloop()
